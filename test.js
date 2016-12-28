62de5227408191c6254e7f42ccd508131dd08b2f
silverwind <me@silverwind.io>
2016-12-28 16:28:58 +0100

    attempt to workaround travis IP issue

--- test.js
+++ test.js
@@ -2,14 +2,44 @@ import {isIPv4, isIPv6} from 'net';
 import test from 'ava';
 import m from './';
 
+// Skip IP tests on Travis because their VMs do not have addresses assigned.
+// See https://docs.travis-ci.com/user/ci-environment/#Networking
+
 test('main', async t => {
-	t.true(isIPv4(await m()));
+	let result;
+	try {
+		result = await m();
+	} catch (err) {
+		if (process.env.TRAVIS === 'true') {
+			return t.true(err.message === 'No interfaces found');
+		}
+	}
+
+	t.true(isIPv4(result));
 });
 
 test('IPv4', async t => {
-	t.true(isIPv4(await m.v4()));
+	let result;
+	try {
+		result = await m.v4();
+	} catch (err) {
+		if (process.env.TRAVIS === 'true') {
+			return t.true(err.message === 'No interfaces found');
+		}
+	}
+
+	t.true(isIPv4(result));
 });
 
 test('IPv6', async t => {
-	t.true(isIPv6(await m.v6()));
+	let result;
+	try {
+		result = await m.v6();
+	} catch (err) {
+		if (process.env.TRAVIS === 'true') {
+			return t.true(err.message === 'No interfaces found');
+		}
+	}
+
+	t.true(isIPv6(result));
 });
