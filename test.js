10df58934389d2358e718ae738d26320782a0e6f
silverwind <me@silverwind.io>
2017-06-25 17:24:58 +0200

    simplify tests

--- test.js
+++ test.js
@@ -2,44 +2,14 @@ import {isIPv4, isIPv6} from 'net';
 import test from 'ava';
 import m from './';
 
-// Skip IP tests on Travis because their VMs do not have addresses assigned.
-// See https://docs.travis-ci.com/user/ci-environment/#Networking
-
 test('main', async t => {
-	let result;
-	try {
-		result = await m();
-	} catch (err) {
-		if (process.env.TRAVIS === 'true') {
-			return t.true(err.message === 'No interfaces found');
-		}
-	}
-
-	t.true(isIPv4(result));
+	t.true(isIPv4(await m()));
 });
 
 test('IPv4', async t => {
-	let result;
-	try {
-		result = await m.v4();
-	} catch (err) {
-		if (process.env.TRAVIS === 'true') {
-			return t.true(err.message === 'No interfaces found');
-		}
-	}
-
-	t.true(isIPv4(result));
+	t.true(isIPv4(await m.v4()));
 });
 
 test('IPv6', async t => {
-	let result;
-	try {
-		result = await m.v6();
-	} catch (err) {
-		if (process.env.TRAVIS === 'true') {
-			return t.true(err.message === 'No interfaces found');
-		}
-	}
-
-	t.true(isIPv6(result));
+	t.true(isIPv6(await m.v6()));
 });
