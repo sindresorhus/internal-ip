4fe9ab40f9df8371dceb8770890b532b0a117972
王麟 <wanglin@ezbuy.com>
2018-03-02 15:26:38 +0800

    Add type defs

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export function v6(): Promise<string>;
+export namespace v6 {
+	export const sync: () => string;
+}
+export function v4(): Promise<string>;
+export namespace v4 {
+	export const sync: () => string;
+}
