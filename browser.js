fa2479c67bd1c4244d7186b85256239029be92cb
Richie Bendall <richiebendall@gmail.com>
2020-11-17 19:10:30 +1300

    Add browser support
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

---
+++ browser.js
@@ -0,0 +1,58 @@
+/* eslint-env browser */
+'use strict';
+import pEvent from 'p-event';
+
+const ipv4 = async ({isSecondTry = false} = {}) => {
+	const peerConnection = new RTCPeerConnection({iceServers: []});
+
+	peerConnection.createDataChannel('');
+	peerConnection.createOffer(peerConnection.setLocalDescription.bind(peerConnection), () => { });
+
+	const {candidate} = await pEvent(peerConnection, 'icecandidate', {
+		timeout: 10000
+	});
+
+	peerConnection.close();
+
+	if (candidate && candidate.candidate) {
+		const result = candidate.candidate.split(' ')[4];
+		if (result.endsWith('.local')) {
+			if (isSecondTry) {
+				return undefined;
+			}
+
+			const inputDevices = await navigator.mediaDevices.enumerateDevices();
+			const inputDeviceTypes = new Set(inputDevices.map(({kind}) => kind));
+
+			const constraints = {};
+
+			if (inputDeviceTypes.has('audioinput')) {
+				constraints.audio = true;
+			} else if (inputDeviceTypes.has('videoinput')) {
+				constraints.video = true;
+			} else {
+				return undefined;
+			}
+
+			const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+			mediaStream.getTracks().forEach(track => track.stop());
+			return ipv4({isSecondTry: true});
+		}
+
+		return result;
+	}
+};
+
+export const v4 = async () => {
+	try {
+		return await ipv4();
+	} catch {
+		return undefined;
+	}
+};
+
+v4.sync = () => undefined;
+
+export const v6 = () => undefined;
+
+v6.sync = () => undefined;
--- readme.md
+++ readme.md
@@ -30,7 +30,7 @@ console.log(internalIp.v4.sync())
 
 The module returns the address of the internet-facing interface, as determined from the default gateway. When the address cannot be determined for any reason, `undefined` will be returned.
 
-The module relies on operating systems tools. On Linux and Android, the `ip` command must be available, which depending on distribution might not be installed by default. It is usually provided by the `iproute2` package.
+The module relies on operating systems tools. On Linux and Android, the `ip` command must be available, which depending on distribution might not be installed by default. It is usually provided by the `iproute2` package. Only `.v4()` works in [modern browsers](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection#Browser_compatibility).
 
 ## Related
 
